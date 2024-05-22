7948c444a11827abf3bc1805c1afc47350d9019a
Thiago-Souzaa <tssti@outlook.com>
2024-05-22 12:25:40 -0300

    primeiro comit

---
+++ src/main/java/com/curso/springbooth2jpaPostgreSQL/SpringbootH2JpaPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springbooth2jpaPostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootH2JpaPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootH2JpaPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/springbooth2jpaPostgreSQL/SpringbootH2JpaPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springbooth2jpaPostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2JpaPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
